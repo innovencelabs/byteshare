6dcbfe5b0a92c63097ce5be7e27386b87a336a33
Ambuj Raj <thecoderguy@outlook.com>
2024-01-18 21:41:11 +0530

    removed svg in auth ui

--- ui/src/app/auth/signup/page.tsx
+++ ui/src/app/auth/signup/page.tsx
@@ -43,7 +43,7 @@ export default function SignupPage() {
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
@@ -54,7 +54,7 @@ export default function SignupPage() {
               className="mr-2 h-6 w-6"
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
+            </svg> */}
             ByteShare
           </div>
           <div className="relative z-20 mt-auto">
