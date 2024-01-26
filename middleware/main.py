3f22674d444066b2b6c0202285f6cd246297f956
Ambuj Raj <thecoderguy@outlook.com>
2024-01-26 23:26:59 +0530

    added continue id, multi file upload support

--- middleware/main.py
+++ middleware/main.py
@@ -54,6 +54,17 @@ class InitiateUpload(BaseModel):
     creator_ip: str
 
 
+class ContinueUpload(BaseModel):
+    file_name: str
+    continue_id: str
+    creator_email: str
+    creator_ip: str
+
+
+class PostUpload(BaseModel):
+    file_names: list
+
+
 @app.get("/health")
 def health_check():
     """
@@ -105,15 +116,15 @@ def initiate_upload_return_upload_url(body: InitiateUpload, request: Request):
     - file_name: name of the file to be uploaded
     - creator_email: email of the creator
     - creator_ip: ip address of the creator
-    - content-length: file size of the uploaded file
+    - Fontent-Length: file size of the uploaded file
 
     Returns:
-    - Upload URL for upload
+    - Upload URL for upload and Upload id
     """
 
-    content_length = request.headers.get("content-length")
+    content_length = int(request.headers.get("File-Length"))
     if content_length is None:
-        raise HTTPException(status_code=400, detail="Content-Length header not found")
+        raise HTTPException(status_code=400, detail="file-Length header not found")
 
     max_file_size = 2 * 1024 * 1024 * 1024  # 2GB
 
@@ -122,6 +133,7 @@ def initiate_upload_return_upload_url(body: InitiateUpload, request: Request):
 
     file_name = body.file_name
     upload_id = uuid.uuid4().hex
+    continue_id = uuid.uuid4().hex
     file_path = upload_id + "/" + file_name
     expiration_time = 10800
     upload_url = storage.generate_upload_url(file_path, expiration_time)
@@ -135,7 +147,9 @@ def initiate_upload_return_upload_url(body: InitiateUpload, request: Request):
         "creator_ip": body.creator_ip,
         "download_count": 0,
         "max_download": 5,
-        "storage_file_name": file_name,
+        "continue_id": continue_id,
+        "total_size": content_length,
+        "storage_file_names": [file_name],
         "storage_qr_name": "",
         "expires_at": "",
         "updated_at": "",
@@ -143,17 +157,83 @@ def initiate_upload_return_upload_url(body: InitiateUpload, request: Request):
     }
     dynamodb.create_item(upload_metadata)
 
-    return {"upload_url": upload_url, "upload_id": upload_id}
+    return {
+        "upload_url": upload_url,
+        "upload_id": upload_id,
+        "continue_id": continue_id,
+    }
+
+
+@app.post("/initiateUpload/{upload_id}")
+def initiate_upload_with_upload_id_return_upload_url(
+    body: ContinueUpload, upload_id: str, request: Request
+):
+    """
+    Initiate upload to Storage for existing upload_id.
+    checks for file size under limit for current user type, creates upload URL for upload and add to DB.
+    Stores the file as <UPLOAD_ID>/<FILE_NAME> in Storage
+
+    Parameters:
+    - upload_id: upload id of the upload process
+    - file_name: name of the file to be uploaded
+    - continue_id: continue id to continue multifile upload
+    - creator_email: email of the creator
+    - creator_ip: ip address of the creator
+    - content-length: file size of the uploaded file
+
+    Returns:
+    - Upload URL for upload
+    """
+
+    content_length = int(request.headers.get("File-Length"))
+    if content_length is None:
+        raise HTTPException(status_code=400, detail="File-Length header not found")
+
+    max_file_size = 2 * 1024 * 1024 * 1024  # 2GB
+
+    upload_metadata = dynamodb.read_item({"upload_id": upload_id})
+    if upload_metadata == None:
+        raise HTTPException(status_code=400, detail="Upload ID not valid")
+    if upload_metadata["continue_id"] != body.continue_id:
+        raise HTTPException(status_code=400, detail="Continue ID not valid")
+
+    if int(content_length) + int(upload_metadata["total_size"]) > max_file_size:
+        raise HTTPException(status_code=400, detail="File size exceeds the limit")
+
+    file_name = body.file_name
+    file_path = upload_id + "/" + file_name
+    expiration_time = 10800
+    upload_url = storage.generate_upload_url(file_path, expiration_time)
+    storage_file_names = list(
+        upload_metadata["storage_file_names"]
+        if upload_metadata["storage_file_names"] != None
+        else []
+    )
+    storage_file_names.append(file_name)
+    content_length = content_length + int(upload_metadata["total_size"])
+
+    time_now = datetime.now(timezone.utc)
+
+    keys = {"upload_id": upload_id}
+    update_data = {
+        "total_size": content_length,
+        "storage_file_names": storage_file_names,
+        "updated_at": time_now.isoformat(),
+    }
+    dynamodb.update_item(keys, update_data)
+
+    return {"upload_url": upload_url}
 
 
 @app.post("/postUpload/{upload_id}")
-def post_upload_return_link_qr(upload_id: str):
+def post_upload_return_link_qr(body: PostUpload, upload_id: str):
     """
     Post upload to Storage.
     Update status to DB, check for the file present in Storage, generate sharable link and QR
 
     Parameters:
     - upload_id: upload id of the upload process
+    - file_name: name of the file uploaded
 
     Returns:
     - Sharable Link and QR code of frontend page
@@ -162,19 +242,21 @@ def post_upload_return_link_qr(upload_id: str):
     upload_metadata = dynamodb.read_item({"upload_id": upload_id})
     if upload_metadata == None:
         raise HTTPException(status_code=400, detail="Upload ID not valid")
-    file_name = upload_metadata["storage_file_name"]
-    file_path = upload_id + "/" + file_name
-    upload_expiration_time = 604800  # 7 days
 
-    # Check for file present in Storage
-    is_file_present = storage.is_file_present(file_path)
-    if not is_file_present:
-        raise HTTPException(status_code=400, detail="Upload not found")
+    file_names = body.file_names
+    for file_name in file_names:
+        file_path = upload_id + "/" + file_name
+
+        # Check for file present in Storage
+        is_file_present = storage.is_file_present(file_path)
+        if not is_file_present:
+            raise HTTPException(status_code=400, detail="Upload not found")
 
     # Generate share link
     file_url = web_base_url + "/share/" + upload_id
 
     time_now = datetime.now(timezone.utc)
+    upload_expiration_time = 604800  # 7 days
     expires_at = time_now + timedelta(seconds=upload_expiration_time)
 
     # Generate QR code
@@ -192,7 +274,7 @@ def post_upload_return_link_qr(upload_id: str):
     temp_qr_path = "{}.png".format(upload_id)
     img.save(temp_qr_path)
 
-    qr_name = "QRCode.png"
+    qr_name = "QRCode_" + upload_id + ".png"
     qr_storage_file_name = upload_id + "/" + qr_name
 
     # Upload the QR code to Storage
@@ -231,10 +313,13 @@ def get_file_url_return_name_link(upload_id: str):
     - File name and download url
     """
 
+    file_urls = []
     time_now = datetime.now(timezone.utc).isoformat()
     upload_metadata = dynamodb.read_item({"upload_id": upload_id})
     if upload_metadata == None:
         raise HTTPException(status_code=400, detail="Upload ID not valid")
+    if upload_metadata["status"] != StatusEnum.uploaded.name:
+        raise HTTPException(status_code=400, detail="It is an incomplete upload")
 
     expires_at = upload_metadata["expires_at"]
     if time_now > expires_at:
@@ -245,21 +330,23 @@ def get_file_url_return_name_link(upload_id: str):
     if download_count >= max_count:
         raise HTTPException(status_code=400, detail="Download limit exceeded")
 
-    file_name = upload_metadata["storage_file_name"]
-    file_path = upload_id + "/" + file_name
-    download_expiration_time = 21600  # 6 hours
+    file_names = set(upload_metadata["storage_file_names"])
+    for file_name in file_names:
+        file_path = upload_id + "/" + file_name
+        download_expiration_time = 21600  # 6 hours
 
-    # Generate share download link
-    file_url = storage.generate_download_url(file_path, download_expiration_time)
+        # Generate share download link
+        file_url = storage.generate_download_url(file_path, download_expiration_time)
+        file_urls.append(file_url)
 
     keys = {"upload_id": upload_id}
     update_data = {
         "download_count": download_count + 1,
-        "updated_at": time_now.isoformat(),
+        "updated_at": time_now,
     }
     dynamodb.update_item(keys, update_data)
 
-    return {"file_name": file_name, "url": file_url}
+    return {"file_names": list(file_names), "urls": file_urls}
 
 
 def _get_file_extension(file_name):
