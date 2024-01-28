35b2609418f884dcfa3a3a5390580b0ae26b0da0
Ambuj Raj <thecoderguy@outlook.com>
2024-01-28 17:25:00 +0530

    added copy icon to original state after 2 sec

--- ui/src/app/(pages)/page.tsx
+++ ui/src/app/(pages)/page.tsx
@@ -113,6 +113,7 @@ export default function Home() {
   const handleCopy = async () => {
     await navigator.clipboard.writeText(shareURL)
     setIsCopied(true)
+    setTimeout(() => setIsCopied(false), 2000)
   }
 
   const handleUploadSubmit = async (event) => {
@@ -406,7 +407,7 @@ export default function Home() {
         onClick={() => window.open(`https://twitter.com/_ambujraj`, '_blank')}
         className="fixed bottom-4 right-0 md:right-0 lg:right-0 xl:right-0 my-0 bg-white text-black rounded-none py-2 px-2 text-xs flex items-center focus:outline-none hover:bg-white z-10"
       >
-        <TwitterLogoIcon className="mr-1 w-4 h-4" />
+        {/* <TwitterLogoIcon className="mr-1 w-4 h-4" /> */}
         <span>@_ambujraj</span>
       </button>
       <div className="absolute inset-0">
