9ffe7947cbebcfe044147972b11560f50912e805
Ambuj Raj <thecoderguy@outlook.com>
2024-02-02 16:02:29 +0530

    minor placement fix for header

--- ui/src/components/header.tsx
+++ ui/src/components/header.tsx
@@ -17,7 +17,7 @@ export const Header = ({ authorised, statusLoaded }) => {
   }, [])
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 z-10">
-      <div className="flex flex-wrap items-center justify-between mx-10 p-4">
+      <div className="flex flex-wrap items-center justify-between md:mx-10 p-4 sm:mx-auto">
         <Link
           href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -81,19 +81,18 @@ export const Header = ({ authorised, statusLoaded }) => {
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                href="/"
-                className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
                 href="/features"
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Features
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/help"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Help
               </Link>
             </li>
             <li>
