ae14147cee90cce1a47bb63ae9c0936f1b7d8196
Ambuj Raj <thecoderguy@outlook.com>
2024-03-02 11:55:08 +0530

    only show handle in lg screen

--- ui/src/components/handle.tsx
+++ ui/src/components/handle.tsx
@@ -4,7 +4,7 @@ function TwitterHandle() {
   return (
     <button
       onClick={() => window.open(`https://twitter.com/_ambujraj`, '_blank')}
-      className="fixed bottom-0 right-0 md:right-0 lg:right-0 xl:right-0 my-0 bg-white text-black rounded-s-md py-1.5 px-1.5 text-xxs flex items-center focus:outline-none hover:bg-white z-10"
+      className="fixed bottom-0 right-0 md:right-0 lg:right-0 xl:right-0 my-0 bg-white text-black rounded-s-md py-1.5 px-1.5 text-xxs hidden lg:block md:hidden sm:hidden focus:outline-none hover:bg-white z-10"
     >
       {/* <TwitterLogoIcon className="mr-1 w-4 h-4" /> */}
       {/* <div className="mr-1 w-4 h-4">
