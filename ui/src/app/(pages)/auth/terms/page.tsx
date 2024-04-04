45031ddeffb3f159a2efe86b1e4ce9288d2413af
abhisheksharma <sharma.abhi3199@gmail.com>
2024-04-04 13:54:58 +0530

    updated the welcome text and disclaimer

--- ui/src/app/(pages)/auth/terms/page.tsx
+++ ui/src/app/(pages)/auth/terms/page.tsx
@@ -3,13 +3,17 @@ import termsData from 'public/Data/terms-page-data.json';
 
 
 function Terms () {
-  const { welcomeMessage, sections } = termsData.termsOfUse;
+  const { welcomeMessage,paragraphMessage,note, sections } = termsData.termsOfUse;
 
   return (
     <div className="p-5">
       <header className="container max-width-lg mt-4 mb-4">
-        <div className="text-center line-height-xl v-space-md margin-bottom-md text-3xl leading-8">
+        <div className="flex flex-col place-items-center justify-center line-height-xl v-space-md margin-bottom-md text-3xl leading-8">
             <h1 className='text-4xl font-bold text-center '>{welcomeMessage}</h1>
+          <p className='w-3/2 pt-5 text-lg text-center text-lightgray'>{paragraphMessage}
+          <span className='w-3/2 pt-2 text-lg font-bold text-lighgray-foreground text-center'>{note}</span>
+          </p>
+          
         </div>
     </header>   
       {sections.map((section, index) => (
